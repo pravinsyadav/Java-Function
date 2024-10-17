43fbe22aa30a1eb76f22280525487370ccf085ba
pravinsyadav <pravinsy2003@gmail.com>
2024-10-17 23:00:50 +0530

    fibonacchi series

---
+++ Fibonacchi.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Fibonacchi {
+
+    public static void printFibo(int n){
+        int a = 0;
+        int b = 1;
+
+        System.out.println("print upto "+n +" terms" );
+        for(int i = 1; i<=n; i++){
+            System.out.print(a +" ");
+            int c = a+b;
+            a = b;
+            b = c;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printFibo(n);
+    }
+}
